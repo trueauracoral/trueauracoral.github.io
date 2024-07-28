0f36dc63acf8b5a8b89c28ec5f1c60befbfa8ba1
zortazert <zortazert@matthewevan.xyz>
2024-07-27 19:24:56 -0500

    write to files with utf-8 and WIP rss

--- website.py
+++ website.py
@@ -7,7 +7,10 @@ from dateutil.relativedelta import relativedelta
 from orgpython import to_html
 import json
 from bs4 import BeautifulSoup
+from xml.dom import minidom 
 
+websiteTitle = "ZortaZert"
+websiteUrl = "https://trueauracoral.github.io"
 articlesDir = os.path.join(os.getcwd(), "blog/org")
 articlesWebDir = "./blog/org/"
 blogDir = os.path.join(os.getcwd(), "blog")
@@ -32,7 +35,6 @@ def getfiles(dirpath):
 articles = getfiles(articlesDir)
 blogData = []
 
-
 with open("top.html", "r") as file:
     topHTML = file.read()
 
@@ -41,7 +43,7 @@ with open("bottom.html", "r") as file:
     soup = BeautifulSoup(bottomHTML, "html.parser")
 
     genDateHTML = soup.find("p", {"id": "genDate"})
-    genDateHTML.replace_with(f"<p>Last generated: {currentDate}<p>")
+    genDateHTML.replace_with(f"Last generated: {currentDate}")
 
     copyrightHTML = soup.find("p", {"id": "copyright"})
     newCopyright = BeautifulSoup(f"© ZortaZert 2021-{currentDate.year}")
@@ -63,7 +65,8 @@ def main():
         path = os.path.join(articlesDir, article)
         date = fileGetDate(article)
         with open(path, "r") as file:
-            title = ' '.join(file.read().splitlines()[0].split(" ")[1:])
+            articleText = file.read()
+            title = ' '.join(articleText.splitlines()[0].split(" ")[1:])
 
         blogData.append({
             "file": path,
@@ -74,15 +77,18 @@ def main():
         })
 
     # Export the Org documents with org-python
-    def exporter(file, export):
+    def exporter(file, export, write=False):
         #command = f'pandoc -s -c {os.path.join(articlesDir, "index.css")} {file} -o {export}.html --highlight-style=tango'
         #print(command)
         #os.system(command)
         with open(file, "r", encoding="utf8") as f:
             articleText = str(f.read())
         orgToHTML = to_html(articleText, toc=False, offset=0, highlight=True)
-        with open(export, "w", encoding="utf8") as f:
-            f.write(htmlify(orgToHTML))
+        if (write):
+            return orgToHTML
+        else:
+            with open(export, "w", encoding="utf8") as f:
+                f.write(htmlify(orgToHTML))
 
         print(f"Exported: {file}")
 
@@ -123,10 +129,79 @@ def main():
     blogHTML += "\n</ul>"
 
     blogPage = os.path.join(blogDir, "index.html")
-    with open(blogPage, "w") as file:
+    with open(blogPage, "w", encoding="utf-8") as file:
         file.write(htmlify(blogHTML))
     
     print(blogData)
+    ## Create RSS for blog
+    rss = minidom.Document()
+    print(rss.getElementsByTagName("xml"))
+    rssElement = rss.createElement('rss')
+    rssElement.setAttribute('version', '2.0')
+    rss.appendChild(rssElement)
+    channel = rss.createElement('channel')
+    rssElement.appendChild(channel)
+    title = rss.createElement('title')
+    title.appendChild(rss.createTextNode(websiteTitle))
+    channel.appendChild(title)
+    language = rss.createElement('language')
+    language.appendChild(rss.createTextNode("en-us"))
+    channel.appendChild(language)
+    link = rss.createElement('link')
+    link.appendChild(rss.createTextNode(urljoin(websiteUrl, "rss.xml")))
+    channel.appendChild(link)
+    
+    #atomLink = rss.createElement('atom:link')
+    #atomLink.setAttribute('href', 'https://monstro1.com/rss.xml')
+    #atomLink.setAttribute('rel', 'self')
+    #atomLink.setAttribute('type', 'application/rss+xml')
+    #channel.appendChild(atomLink)
+    
+    image = rss.createElement('image')
+    channel.appendChild(image)
+    imageTitle = rss.createElement('title')
+    imageTitle.appendChild(rss.createTextNode(websiteTitle))
+    image.appendChild(imageTitle)    
+    imageUrl = rss.createElement('url')
+    imageUrl.appendChild(rss.createTextNode(urljoin(websiteTitle, "/img/icons/chadoku.ico")))
+    image.appendChild(imageUrl)    
+    imageLink = rss.createElement('link')
+    imageLink.appendChild(rss.createTextNode(urljoin(websiteUrl, "rss.xml")))
+    image.appendChild(imageLink)
+
+    for article in blogData:
+        item = rss.createElement('item')
+        channel.appendChild(item)
+        itemTitle = rss.createElement('title')
+        itemTitle.appendChild(rss.createTextNode(article['title']))
+        item.appendChild(itemTitle)
+        
+        guid = rss.createElement('guid')
+        guid.appendChild(rss.createTextNode(article['url']))
+        item.appendChild(guid)
+        
+        itemLink = rss.createElement('link')
+        itemLink.appendChild(rss.createTextNode(urljoin(websiteUrl, f"/blog/{article['url']}")))
+        item.appendChild(itemLink)
+        
+        pubDate = rss.createElement('pubDate')
+        pubDate.appendChild(rss.createTextNode(article['date'].strftime("%a, %d %b %Y %H:%M:%S %z")))
+        item.appendChild(pubDate)
+        
+        with open(article['file'], 'r') as file:
+            articleText = file.read()
+            articleText = to_html(articleText, toc=False, offset=0, highlight=True).replace('\n','')
+
+        description = rss.createElement('description')
+        description.appendChild(rss.createTextNode(articleText))
+        item.appendChild(description)
+    
+    xml_str = rss.toprettyxml(indent ="  ")  
+    #print(xml_str)
+
+    with open("rss.xml", "w", encoding="utf-8") as f: 
+        f.write(xml_str) 
+        print("Exported: rss.xml")
     
 if __name__ == "__main__":
     main()
